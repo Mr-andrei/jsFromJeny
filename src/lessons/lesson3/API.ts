86001c9414c549505d508668a5a195bd7af6ea68
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2022-01-23 17:10:39 +0300

    next commit

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
+const key = '81db0e02';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
+    params: {
+        apikey: key
+    }
 };
-const key = '';
 const axiosInstance = axios.create(configOMB);
-
+   // ?apikey=[ваш ключ]&
+// http://www.omdbapi.com/?t=r&plot=full
+// http://www.omdbapi.com/?i=tt3896198&apikey=81db0e02
 const API = {
     searchFilmsByTitle: (title: string) => {
+       return  axiosInstance.get(``,{params:{t:title}})
     },
     searchFilmsByType: (title: string, type: string) => {
     }
--- src/lessons/lesson3/Lesson3.tsx
+++ src/lessons/lesson3/Lesson3.tsx
@@ -5,12 +5,23 @@ import './lesson_3';
 const Lesson3 = () => {
     const [searchName, setSearchName] = useState('');
     const [serachResult, setSerachResult] = useState('');
+    const [images, setImages] = useState("")
+    const [title, setTitle] = useState("")
     const [searchNameByType, setSearchNameByType] = useState('');
     const [serachResultByType, setSerachResultByType] = useState('');
 
     const searchFilm = () => {
         API.searchFilmsByTitle(searchName)
+            .then(data => {
+                setSerachResult(data.data.Actors)
+                    return data
+            })
+            .then(data => {
+                setImages(data.data.Poster)
+                return data
+            }).then(data => setTitle(data.data.Title))
     };
+
 
     const searchByType = (e: React.MouseEvent<HTMLButtonElement>) => {
         const type: string = e.currentTarget.dataset.t ? e.currentTarget.dataset.t : '';
@@ -25,7 +36,9 @@ const Lesson3 = () => {
                 <input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)}/>
                 <button onClick={searchFilm}>Search</button>
                 <div>
-                    {serachResult}
+                   <span>{title}</span>
+                   <div> {serachResult} </div>
+                    <img src={images} alt=""/>
                 </div>
             </div>
 
